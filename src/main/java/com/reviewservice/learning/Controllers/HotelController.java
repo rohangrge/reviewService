736758cb7a6661689a220378c71f3559a4ee5c97
rohangrge <rohangrge@gmail.com>
2022-04-28 23:43:48 +0530

    add thymeleaf ui elements

--- src/main/java/com/reviewservice/learning/Controllers/HotelController.java
+++ src/main/java/com/reviewservice/learning/Controllers/HotelController.java
@@ -7,9 +7,13 @@ import com.reviewservice.learning.Services.ReviewService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.List;
+
+@Controller
 @RequestMapping("api/hotels")
 @Slf4j
 public class HotelController {
@@ -33,5 +37,12 @@ public class HotelController {
         ApiResponse apiResponse = new ApiResponse();
         hotelService.addHotelToDb(hotels);
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public String getHotels(Model model) {
+        List<Hotels> hotels = hotelService.getAllHotels();
+        model.addAttribute("hotels" ,hotels);
+        return "hotels";
     }
 }
